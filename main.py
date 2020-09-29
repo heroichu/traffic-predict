3dc81a795882a2eb81a8a670c1d1bb8582fda7d2
kagohu <heroichu@gmail.com>
2020-09-29 13:24:17 +0900

    traffci predict

---
+++ main.py
@@ -0,0 +1,90 @@
+import os
+import json
+import time
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from core.data_processor import DataLoader
+from core.model import Model
+
+
+
+def plot_results(predicted_data, true_data):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    plt.plot(predicted_data, label='Prediction')
+    plt.legend()
+    plt.show()
+    plt.savefig("./images/compare_pbp_0929.png", dpi=10000)
+
+
+
+def plot_results_multiple(predicted_data, true_data, prediction_len):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+	# Pad the list of predictions to shift it in the graph to it's correct start
+    for i, data in enumerate(predicted_data):
+        padding = [None for p in range(i * prediction_len)]
+        plt.plot(padding + data, label='Prediction')
+        plt.legend()
+    plt.show()
+
+def main():
+    configs = json.load(open('config.json', 'r'))
+
+    #create folder for save model params
+    if not os.path.exists(configs['model']['save_dir']): os.makedirs(configs['model']['save_dir'])
+
+    data = DataLoader(
+        os.path.join('data', configs['data']['filename']),
+        configs['data']['train_test_split'],
+        configs['data']['columns']
+    )
+
+    #plot true data
+    #plot_results(data.data_train,True)
+
+    #train model
+    model = Model()
+    model.build_model(configs)
+    x, y = data.get_train_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    )
+
+    steps_per_epoch = math.ceil((data.len_train - configs['data']['sequence_length']) / configs['training']['batch_size'])
+    model.train_generator(
+        data_gen=data.generate_train_batch(
+            seq_len=configs['data']['sequence_length'],
+            batch_size=configs['training']['batch_size'],
+            normalise=configs['data']['normalise']
+        ),
+        epochs=configs['training']['epochs'],
+        batch_size=configs['training']['batch_size'],
+        steps_per_epoch=steps_per_epoch,
+        save_dir=configs['model']['save_dir']
+    )
+
+    x_test, y_test = data.get_test_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    )
+
+
+    predictions = model.predict_point_by_point(x_test)
+#    plot_results(predictions, y_test)
+#    print (predictions)
+#    plot_results(predictions, y_test)
+
+
+
+    data1 = pd.DataFrame(predictions)    
+    data1.to_csv('predict.csv')
+    data2 = pd.DataFrame(y_test)
+    data2.to_csv('true.csv')
+
+if __name__ == '__main__':
+    main()
